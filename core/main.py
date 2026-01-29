5f9a53b05ea9e0d30fb4ec9c300940c91b7dee5f
akash <a1920965@gmail.com>
2026-01-29 07:51:06 +0000

    added  CORS configuration

--- core/main.py
+++ core/main.py
@@ -1,27 +1,29 @@
 from typing import List
 from fastapi import FastAPI,Request
-from core.routers import users,auth
+from core.routers import users,auth,complaint,epass
 from core import api_services,models
 from core.exception.handle import register_user_exception_handler
 from fastapi.templating import Jinja2Templates
-templates=Jinja2Templates(directory="core/templates")
-from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 app=FastAPI()
-# models.Base.metadata.create_all(bind=engine)
+
+origin=["10.10.3.178"]
+app.middleware.cors(
+    CORSMiddleware,
+    allow_origin=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 register_user_exception_handler(app)
-
-app.mount("/static", StaticFiles(directory="core/static"), name="static")
-
 @app.get('/')
 def root(request:Request):
-    context = {
-        "request": request,
-        "name": "Akash Gupta",
-        **api_services.weather_api("Mumbai")
-    }
-    return templates.TemplateResponse("app.html",context)
-@app.get('/test')
-def test(request:Request):
-    return templates.TemplateResponse("app.html",{"request":request})
+    return "This is my home page"
+
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(complaint.router)
+app.include_router(epass.router)
