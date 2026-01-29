322b1c2d79d976a4f78dd2819006db27b3200f71
akash <a1920965@gmail.com>
2026-01-29 05:50:50 +0000

    handle bug in jwt exception handling

--- core/o2auth.py
+++ core/o2auth.py
@@ -3,13 +3,8 @@ from fastapi.security import OAuth2PasswordBearer
 from core import schemas
 from core.exception.custom_exceptions import InvalidCredential
 from datetime import datetime,timedelta
-from dotenv import load_dotenv
-from jose import JWTError,jwt
-load_dotenv()
+from jose import JWTError,jwt,ExpiredSignatureError
 import os
-
-
-
 Oauth2=OAuth2PasswordBearer(tokenUrl="login")
 def create_Access_token(user_credentail:schemas.UserAuth):
     to_encode=user_credentail.copy()
@@ -17,13 +12,23 @@ def create_Access_token(user_credentail:schemas.UserAuth):
     to_encode.update({"exp":expire})
     access_token=jwt.encode(to_encode,os.getenv("SECRET_KEY"),algorithm=os.getenv("ALGORITHM"))
     return access_token
-def verify_token(token:str,credentail_exception):
+def verify_token(token: str,credentail_exception):
     try:
-        payload=jwt.decode(token,os.getenv("SECRET_KEY"),algorithms=[os.getenv("ALGORITHM")])
+        payload = jwt.decode(
+            token,
+            os.getenv("SECRET_KEY"),
+            algorithms=[os.getenv("ALGORITHM")]
+        )
         return payload.get('user_id')
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     except JWTError:
         raise credentail_exception()
-
 def get_current_user(token:str=Depends(Oauth2)):
     credentail_exception=InvalidCredential
     return verify_token(token,credentail_exception)
