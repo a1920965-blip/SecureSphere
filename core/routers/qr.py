b278711666480696c841220c46929d30fdee085b
akash <a1920965@gmail.com>
2026-01-29 10:25:53 +0000

    credential execption bug , -f bak

--- core/routers/qr.py
+++ core/routers/qr.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter,status,HTTPException,Request,Depends
 from core import schemas,utils,database,models
+from core.exception.custom_exceptions import Credential_Exception
 from sqlalchemy.orm import Session
 from core.o2auth import create_Access_token,verify_token,get_current_user
 router=APIRouter(tags=["QrCode/Token Valdation"])
@@ -8,5 +9,5 @@ router=APIRouter(tags=["QrCode/Token Valdation"])
 def token_verify(token:str,db:Session=Depends(database.get_db)):
     user=db.get(models.Token,token)
     if user==None:
-        return InvalidCredential()
-    return {"status":True,"messege":"Verifyed user"}
+        return Credential_Exception()
+    return {"status":True,"message":"Verifyed user"}
