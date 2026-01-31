3eb478afb12b3fb70631e07c73203e44fe9bfd70
akash <a1920965@gmail.com>
2026-01-31 19:36:17 +0530

    fix( dead import and directory

--- backend/core/routers/qr_verify.py
+++ backend/core/routers/qr_verify.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter,status,HTTPException,Request,Depends
 from backend.core import schemas,utils,database,models
 from backend.core.exception.custom_exceptions import Credential_Exception
 from sqlalchemy.orm import Session
-from backend.core.o2auth import create_Access_token,verify_token,get_current_user
 
 router=APIRouter(tags=["QrCode/Token Valdation"])
 
